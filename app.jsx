974c68e2c5e7f607d6181ea2a8760b52b03cbd47
ChristopherOlivo <149219728+ChristopherOlivo@users.noreply.github.com>
2023-11-10 18:51:55 -0800

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+import './App.css';
+import TodoComponent from './Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoComponent />
+    </div>
+  );
+}
+
+export default App;
